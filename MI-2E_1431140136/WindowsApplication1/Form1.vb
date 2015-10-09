ec5aca267f200a5869540d70b790ad1c7bb98ccd
muklisnur <entis.hidayat@gmail.com>
2015-10-09 19:04:11 +0700

    KALKULATOR

---
+++ MI-2E_1431140136/WindowsApplication1/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+    Dim a As String
+    Dim b As Integer
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox1.Text = TextBox1.Text & 1
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        TextBox1.Text = TextBox1.Text & 2
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        TextBox1.Text = TextBox1.Text & 3
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        TextBox1.Text = TextBox1.Text & 4
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        TextBox1.Text = TextBox1.Text & 5
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        TextBox1.Text = TextBox1.Text & 6
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        TextBox1.Text = TextBox1.Text & 7
+    End Sub
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        TextBox1.Text = TextBox1.Text & 8
+    End Sub
+
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+        TextBox1.Text = TextBox1.Text & 9
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+        TextBox1.Text = TextBox1.Text & 0
+    End Sub
+
+    Private Sub Button12_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button12.Click
+        a = 2
+        b = TextBox1.Text
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button13_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button13.Click
+        a = 1
+        b = TextBox1.Text
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        Select Case Me.a
+            Case 1
+                TextBox1.Text = Convert.ToString(Me.b - Convert.ToString(TextBox1.Text))
+            Case 2
+                TextBox1.Text = Convert.ToString(Me.b + Convert.ToString(TextBox1.Text))
+        End Select
+    End Sub
+
+    Private Sub Button14_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button14.Click
+        TextBox1.Text = Nothing
+    End Sub
+End Class
